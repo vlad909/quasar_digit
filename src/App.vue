4a754f7861ef87bb77ab14efe2e792f8c033f930
vlad909 <vladislav.g@cronix.ms>
2018-12-19 03:00:15 +0300

    with canvas

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="q-app">
+    <Canvsius></Canvsius>
+    <p>message: {{msg}}</p>
+    <button type="button" @click="msg='dima is a black dude'">кнопка</button>
+  </div>
+</template>
+
+<script>
+  import Canvsius from './layouts/CanvasComponent.vue'
+  // quasar dev -m cordova -T android
+
+  export default {
+    name: 'App',
+    components: {
+      Canvsius
+    },
+    data() {
+      return {
+        msg: 'qqq'
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ src/layouts/CanvasComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <canvas id="sheet"
+          width="400px" height="400px"></canvas>
+</template>
+<script>
+  import 'fabric'
+
+  export default {
+    data() {
+      return {
+        canvas: {}
+      }
+    },
+    mounted() {
+      this.canvas = new fabric.Canvas('sheet')
+      this.canvas.isDrawingMode = true
+      this.canvas.freeDrawingBrush.width = 10
+      this.canvas.freeDrawingBrush.color = "#ff0000";
+    }
+  }
+</script>
+<style>
+  #sheet {
+    border: 1px black solid !important;
+  }
+</style>
