b3a75e8c9e57a94fca2f0b0385a7fc3b9bac6477
Vladislav Gushchin <vladislav.g@cronix.ms>
2018-12-21 10:56:17 +0300

    q-app container (otherwise won't build to prod ver)

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
-  <div class="container">
-      <Canvsius></Canvsius>
-  </div>
+    <div id="q-app">
+        <div class="container">
+            <Canvsius></Canvsius>
+        </div>
+    </div>
 </template>
 
 <script>
-  import Canvsius from './layouts/CanvasComponent.vue'
-  import trainData from './assets/mnistTrain.json'
-  // quasar dev -m cordova -T android
+    import Canvsius from './layouts/CanvasComponent.vue'
+    import trainData from './assets/mnistTrain.json'
+    // quasar dev -m cordova -T android
 
-  export default {
-    name: 'App',
-    components: {
-      Canvsius
-    },
-    data() {
-      return {
-        msg: 'qqq',
-      }
+    export default {
+        name: 'App',
+        components: {
+            Canvsius
+        },
+        data() {
+            return {
+                msg: 'qqq',
+            }
+        }
     }
-  }
 </script>
 
 <style>
-  @import "~bootstrap/dist/css/bootstrap.min.css";
+    @import "~bootstrap/dist/css/bootstrap.min.css";
 </style>
